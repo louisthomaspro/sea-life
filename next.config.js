f1e9e9a8334d73f23ec27b5871ec3b54e67476f3
Louis <louisthomas.pro@gmail.com>
2023-04-17 08:27:40 +0800

    Update cache-control overriden by vercel

--- next.config.js
+++ next.config.js
@@ -13,6 +13,22 @@ const withPWA = require("next-pwa")({
 
 /** @type {import('next').NextConfig} */
 let nextConfig = {
+  // Workaround for minimumCacheTTL
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png|webp)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
+
   // https://react-svgr.com/docs/next/
   webpack(config) {
     const fileLoaderRule = config.module.rules.find((rule) =>
@@ -38,11 +54,11 @@ let nextConfig = {
 
   experimental: {
     //   scrollRestoration: true,
-    turbo: {
-      loaders: {
-        ".svg": ["@svgr/webpack"],
-      },
-    },
+    // turbo: {
+    //   loaders: {
+    //     ".svg": ["@svgr/webpack"],
+    //   },
+    // },
   },
   swcMinify: true,
   reactStrictMode: true,

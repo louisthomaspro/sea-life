31c543e6b0ca02cfb6ef018a8450be8ace021c8a
Louis <louisthomas.pro@gmail.com>
2024-04-29 22:23:44 +0700

    Remove carousel for species list

--- components/low-level-group-card.tsx
+++ components/low-level-group-card.tsx
@@ -5,7 +5,6 @@ import { Prisma } from "@prisma/client"
 
 import { cn } from "@/lib/utils"
 import { Badge } from "@/components/ui/badge"
-import { CarouselSpecies, CarouselSpeciesContent, CarouselSpeciesItem } from "@/components/ui/carousel-species-group"
 import { Icons } from "@/components/ui/icons"
 import ImageLoader from "@/components/ui/image-loader"
 
@@ -22,7 +21,7 @@ type groupWithHighlightedSpecies = Prisma.GroupGetPayload<{
 
 export default function LowLevelGroupCard({ group }: { group: groupWithHighlightedSpecies }) {
   return (
-    <div className="pb-2">
+    <div>
       <Link key={group.id} href={`/explore/${group.slug}`} className="flex items-center pb-1">
         <div className="flex grow gap-2 truncate font-semibold">
           <span>{group.commonNames.en}</span>
@@ -50,49 +49,42 @@ export default function LowLevelGroupCard({ group }: { group: groupWithHighlight
 
 async function SpeciesList({ group }: { group: groupWithHighlightedSpecies }) {
   return (
-    <CarouselSpecies
-      opts={{
-        align: "start",
-      }}
-      className="relative -ml-4 -mr-4 h-20"
-    >
-      <CarouselSpeciesContent className="ml-3 size-full">
+    <div className="relative -ml-4 -mr-4 flex">
+      <div className="no-scrollbar space-x-1 overflow-auto whitespace-nowrap">
         {group.highlightedSpecies.map((species, index) => (
-          <CarouselSpeciesItem
+          <Link
             key={index}
-            className={cn("flex-none pr-1", group.speciesCount <= 5 && index === 4 && "mr-6")}
+            href={`/species/${species.id}`}
+            className={cn(
+              "relative inline-block aspect-[3/2] w-28 overflow-hidden rounded-xl",
+              group.speciesCount <= 5 && index === group.speciesCount - 1 && "!mr-4",
+              index === 0 && "!ml-4"
+            )}
           >
-            <Link
-              href={`/species/${species.id}`}
-              className="relative block aspect-[3/2] size-full overflow-hidden rounded-xl"
-            >
-              <ImageLoader
-                src={species.medias[0]?.url}
-                alt={species.commonNames.en?.[0]!}
-                fill
-                sizes="150px"
-                className="object-cover"
-              />
-            </Link>
-          </CarouselSpeciesItem>
+            <ImageLoader
+              src={species.medias[0]?.url}
+              alt={species.commonNames.en?.[0]!}
+              fill
+              sizes="150px"
+              className="object-cover"
+            />
+          </Link>
         ))}
         {/* View more */}
         {group.speciesCount > 5 && (
-          <CarouselSpeciesItem className="mr-6 flex-none pl-1">
-            <Link
-              href={`/explore/${group.slug}`}
-              className="relative block aspect-[3/2] size-full overflow-hidden rounded-xl border border-gray-200 bg-gray-50"
-            >
-              <div className="flex h-full flex-col items-center justify-center gap-2 text-muted-foreground">
-                <div className="rounded-full bg-gray-200 p-1">
-                  <Icons.chevronRight className="size-3" />
-                </div>
-                <span className="text-xs">See more</span>
+          <Link
+            href={`/explore/${group.slug}`}
+            className="relative !mr-4 inline-block aspect-[3/2] w-28 overflow-hidden rounded-xl border border-gray-200 bg-gray-50"
+          >
+            <div className="flex h-full flex-col items-center justify-center gap-2 text-muted-foreground">
+              <div className="rounded-full bg-gray-200 p-1">
+                <Icons.chevronRight className="size-3" />
               </div>
-            </Link>
-          </CarouselSpeciesItem>
+              <span className="text-xs">See more</span>
+            </div>
+          </Link>
         )}
-      </CarouselSpeciesContent>
-    </CarouselSpecies>
+      </div>
+    </div>
   )
 }

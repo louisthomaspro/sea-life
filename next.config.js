1a0fc8ee067934dc04f102b1af1bd89902c03fb2
Louis Thomas <louisthomas.pro@gmail.com>
2024-05-07 01:03:58 +0700

    Improve PWA manifest

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
+  cacheOnFrontEndNav: false,
+  aggressiveFrontEndNavCaching: false,
   reloadOnOnline: true,
   swcMinify: true,
   // disable: process.env.NODE_ENV === "development",

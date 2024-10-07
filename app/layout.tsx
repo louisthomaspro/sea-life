8ca6a17765a0da59e02928fe75ed3adbc1fffcc2
Louis Thomas <louisthomas.pro@gmail.com>
2024-10-07 20:12:19 +0200

    Prevent zoom on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,10 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   themeColor: "#FFFFFF",
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
